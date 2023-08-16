c440f5c6e63db60e026b4730f73ece655d16d5e1
chaseseidel <chaseseidel16@gmail.com>
2023-08-15 18:38:57 -0700

    Add extra if statement to Education visibility

--- cv-application/src/components/educationSection.jsx
+++ cv-application/src/components/educationSection.jsx
@@ -1,23 +1,28 @@
 export default function Education({ education }) {
-  if (education.length > 0) {
+  const visibleSchools = education.filter((school) => school.visible);
+
+  if (education.length > 0 && visibleSchools.length > 0) {
     return (
       <>
         <ul>
           <h3>Education</h3>
-          {education.map((school) => (
-            <li key={school.name} className="listContent">
-              <div className="info">
-                <p>
-                  {school.start} - {school.end}
-                </p>
-                <p>{school.location}</p>
-              </div>
-              <div className="content">
-                <h4>{school.name}</h4>
-                <p>{school.degree}</p>
-              </div>
-            </li>
-          ))}
+          {education.map(
+            (school) =>
+              school.visible && (
+                <li key={school.name} className="listContent">
+                  <div className="info">
+                    <p>
+                      {school.start} - {school.end}
+                    </p>
+                    <p>{school.location}</p>
+                  </div>
+                  <div className="content">
+                    <h4>{school.name}</h4>
+                    <p>{school.degree}</p>
+                  </div>
+                </li>
+              )
+          )}
         </ul>
       </>
     );
