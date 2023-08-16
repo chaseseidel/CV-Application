3f78d4a31903c0099b060c15282d1ec27535ba3f
chaseseidel <chaseseidel16@gmail.com>
2023-08-15 23:56:02 -0700

    Fix matching key problem

--- cv-application/src/components/experienceSection.jsx
+++ cv-application/src/components/experienceSection.jsx
@@ -1,24 +1,29 @@
 export default function Experience({ experience }) {
-  if (experience.length > 0) {
+  const visibleJobs = experience.filter((job) => job.visible);
+
+  if (experience.length > 0 && visibleJobs.length > 0) {
     return (
       <>
         <ul>
           <h3>Experience</h3>
-          {experience.map((job) => (
-            <li key={job.title} className="listContent">
-              <div className="info">
-                <p>
-                  {job.start} - {job.end}
-                </p>
-                <p>{job.location}</p>
-              </div>
-              <div className="content">
-                <h4>{job.company}</h4>
-                <h5>{job.title}</h5>
-                <p>{job.description}</p>
-              </div>
-            </li>
-          ))}
+          {experience.map(
+            (job) =>
+              job.visible && (
+                <li key={job.id} className="listContent">
+                  <div className="info">
+                    <p>
+                      {job.start} - {job.end}
+                    </p>
+                    <p>{job.location}</p>
+                  </div>
+                  <div className="content">
+                    <h4>{job.company}</h4>
+                    <h5>{job.title}</h5>
+                    <p>{job.description}</p>
+                  </div>
+                </li>
+              )
+          )}
         </ul>
       </>
     );
